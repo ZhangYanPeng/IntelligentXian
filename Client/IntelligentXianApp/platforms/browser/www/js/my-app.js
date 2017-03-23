fd91f990a5affd2fa79f64fad1bd65fdd744c48d
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-03-23 17:20:29 +0800

    aa

--- Client/IntelligentXianApp/platforms/browser/www/js/my-app.js
+++ Client/IntelligentXianApp/platforms/browser/www/js/my-app.js
@@ -1,5 +1,19 @@
 // Initialize app
-var myApp = new Framework7();
+var myApp = new Framework7({
+        // Default title for modals
+        modalTitle: 'My App',
+     
+        // If it is webapp, we can enable hash navigation:
+        pushState: true,
+     
+        // Hide and show indicator during ajax requests
+        onAjaxStart: function (xhr) {
+            myApp.showIndicator();
+        },
+        onAjaxComplete: function (xhr) {
+            myApp.hideIndicator();
+        }
+  }); 
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
